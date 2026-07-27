a4358ac5dc3ba6c2d904166f9c20643c33f8e176
PratesB <brunopratesjus@gmail.com>
2026-07-27 08:33:49 +0000

    implement user dashboard and add message feedback to login page

--- users/urls.py
+++ users/urls.py
@@ -8,4 +8,5 @@ app_name = 'users'
 
 urlpatterns = [
     path('', views.login, name='login'),
+    path('dashboard/', views.dashboard, name='dashboard'),
 ]
--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login as auth_login
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 
 
@@ -14,9 +15,15 @@ def login(request):
         
         if user is not None:
             auth_login(request, user)
-            return HttpResponse('User logged successfully!')
+            return redirect('users:dashboard')
         else:
             messages.error(request, 'Invalid credentials')
             return redirect('users:login')
 
     return render(request, 'users/login.html')
+
+
+
+@login_required(login_url='users:login')
+def dashboard(request):
+    return render(request, 'users/dashboard.html')
