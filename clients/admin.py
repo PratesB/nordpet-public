54813cce09454133a94a2473e1ae4ded4798ff5d
PratesB <brunopratesjus@gmail.com>
2026-07-28 05:48:57 +0000

    implement Client and Animal models with admin interface registration

--- clients/admin.py
+++ clients/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Client, Animal
 
-# Register your models here.
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'phone')
+    list_filter = ('created_at', 'updated_at')
+    search_fields = ('name', 'email', 'phone')
+    ordering = ('-created_at',)
+    
+
+@admin.register(Animal)
+class AnimalAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'specie', 'breed', 'gender', 'owner', 'date_of_birth', 'is_estimated_dob')
+    list_filter = ('specie', 'gender', 'owner', 'is_estimated_dob')
+    search_fields = ('name', 'specie', 'breed', 'gender', 'owner__name', 'is_estimated_dob')
+    ordering = ('-created_at',)
--- clients/models.py
+++ clients/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+
+class Client(models.Model):
+    
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=20)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.name} - {self.email}'
+
+
+
+
+class Animal(models.Model):
+
+    SPECIE_CHOICES = [
+        ('dog', 'Dog'),
+        ('cat', 'Cat'),
+        ('bird', 'Bird'),
+        ('reptile', 'Reptile'),
+        ('rabbit', 'Rabbit'),
+        ('hamsters', 'Hamsters'),
+        ('other', 'Other'),
+    ]
+
+    GENDER_CHOICES = [
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('U', 'Unknown'),
+    ]
+
+    owner = models.ForeignKey(Client, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    specie = models.CharField(max_length=100, choices=SPECIE_CHOICES)
+    breed = models.CharField(max_length=100)
+    gender = models.CharField(max_length=100, choices=GENDER_CHOICES, default='U')
+    date_of_birth = models.DateField(null=True, blank=True)
+    is_estimated_dob = models.BooleanField(default=False)
+    photo = models.ImageField(upload_to='pets/', null=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.name} - {(self.specie)} | {self.owner}'
